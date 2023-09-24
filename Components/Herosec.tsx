3cd1c5db3935cfdf77299af8128610bd1bcaedaa
Kevenn23 <kevenwen18@gmail.com>
2023-09-24 13:43:57 +0700

    Update Herosec.tsx

--- Components/Herosec.tsx
+++ Components/Herosec.tsx
@@ -10,9 +10,9 @@ const Herosec = () => {
 
 
   return (
-    <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+    <div className="hero">
         <div className="flex-1 pt-36 padding-x">
-            <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+            <h1 className="hero__title">
             Discover the future of car rentals!
             </h1>
 
@@ -26,13 +26,13 @@ const Herosec = () => {
                 handleClick={handleScroll}
             />
         </div>
-        <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
-        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0">
+        <div className="hero__image-container">
+        <div className="hero__image">
             <Image src="/hero.png" alt="Hero-img" 
             fill className="object-contain"/>
         </div>
 
-            <div className="absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden">
+            <div className="hero__image-overlay">
             </div>
 
         </div>
