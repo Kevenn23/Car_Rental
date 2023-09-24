0de73d072872baa667da1e8c0e8cff6f71762d00
Kevenn23 <kevenwen18@gmail.com>
2023-09-24 12:51:52 +0700

    work in progress

---
+++ Components/Herosec.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Image from "next/image";
+import { CustomButton } from ".";
+
+const Herosec = () => {
+    const handleScroll = () =>{
+
+    }
+
+
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+            Discover the future of car rentals!
+            </h1>
+
+            <p className="hero__subtitle">
+            Streamline your rental process with our quick and easy booking service
+            </p>
+
+            <CustomButton 
+                title="Explore Cars"
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+        </div>
+        <div className="hero__image-container">
+        <div className="hero__image">
+            <Image src="/hero.png" alt="Hero-img" 
+            fill className="object-contain"/>
+        </div>
+
+            <div className="hero__image-overlay">
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Herosec
---
+++ Components/index.ts
@@ -0,0 +1,20 @@
+import Herosec from "./Herosec";
+import CustomButton from "./custombutton";
+import NavBar from "./NavBar";
+import { Footer } from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+
+
+export{
+    Herosec,
+    CustomButton,
+    Footer,
+    NavBar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+}
