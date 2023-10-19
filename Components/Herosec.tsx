6907a172ea224b55d0418b301b03ec5ca748997b
Kevenn23 <kevenwen18@gmail.com>
2023-10-20 03:29:16 +0700

    Update Herosec.tsx

--- Components/Herosec.tsx
+++ Components/Herosec.tsx
@@ -11,29 +11,32 @@ const Herosec = () => {
 
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
-        </h1>
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+            Discover the future of car rentals!
+            </h1>
 
-        <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
-        </p>
+            <p className="hero__subtitle">
+            Streamline your rental process with our quick and easy booking service
+            </p>
 
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        />
-      </div>
-      <div className="hero__image-container">
-        <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+            <CustomButton 
+                title="Explore Cars"
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+        </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="Hero-img" 
+                fill className="object-contain"/>
+            </div>
+
+            <div className="hero__image-overlay">
+            </div>
+
         </div>
 
-        <div className="hero__image-overlay" />
-      </div>
     </div>
   )
 }
