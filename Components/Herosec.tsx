22bda49f8484f0c3262d71413382225ad3a66319
Kevenn23 <kevenwen18@gmail.com>
2023-10-20 02:46:50 +0700

    Update Herosec.tsx

--- Components/Herosec.tsx
+++ Components/Herosec.tsx
@@ -12,11 +12,11 @@ const Herosec = () => {
   return (
     <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
         <div className="flex-1 pt-36 padding-x">
-            <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+            <h1 className="hero__title">
             Discover the future of car rentals!
             </h1>
 
-            <p className="text-[27px] text-black-100 font-light mt-5">
+            <p className="hero__subtitle">
             Streamline your rental process with our quick and easy booking service
             </p>
 
